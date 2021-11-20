b569815ad35c6a57e32606edf015fcf05aea72c8
Kaito Kobayashi <kkaitoko.abckk@gmail.com>
2021-11-20 18:16:11 -0500

    part9_placeNewPostOnHomePage

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-let allPost =[]
+let allPost = []
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,37 +13,45 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
-app.get("/", function(req,res){
-  res.render("home", {startingContent:homeStartingContent });
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    allPost: allPost
+  });
 });
 
-app.get("/about", function(req,res){
-  res.render("about", {aboutContent:aboutContent });
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
-app.get("/contact", function(req,res){
-  res.render("contact", {contactContent:contactContent });
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
 
 
-app.get("/compose", function(req,res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req,res){
+app.post("/compose", function(req, res) {
   let title = req.body.postTitle;
   let body = req.body.postBody;
 
   let post = {
     title: title,
-    body:body
+    body: body
   };
   allPost.push(post);
-  console.log(allPost);
   res.redirect("/");
 
 })
